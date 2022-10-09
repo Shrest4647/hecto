dab16512c0bc01ed88d346c48f4df2af3208e96d
SUNIL_WSL <shrest464760@gmail.com>
2022-10-09 23:36:18 +0545

    After code clean up for terminal

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,5 @@
 use crate::Terminal;
-use std::io::{self, stdin, stdout, Write};
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
 
 pub struct Editor {
    should_quit: bool,
@@ -11,7 +8,6 @@ pub struct Editor {
 
 impl Editor {
  pub fn run(&mut self) {
-   let _stdout = stdout().into_raw_mode().unwrap();
    
    loop {
       if let Err(error) = self.refresh_screen() {
@@ -34,26 +30,26 @@ impl Editor {
  }
 
  fn refresh_screen(&self) -> Result<(), std::io::Error> {
-   print!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
-   
+   Terminal::clear_screen();
+   Terminal::cursor_position(0, 0);   
    if self.should_quit {
       println!("\n Goodbye. \r");
    } else {
       self.draw_rows();
-      print!("{}", termion::cursor::Goto(1,1))
+      Terminal::cursor_position(0, 0);
    }
 
-   io::stdout().flush()
+   Terminal::flush()
  }
 
  fn draw_rows(&self) {
-   for _ in 0..self.terminal.size().height {
+   for _ in 0..self.terminal.size().height - 1 {
       println!("~\r");
    }
  }
 
  fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-   let pressed_key = Self::read_key()?;
+   let pressed_key = Terminal::read_key()?;
    match pressed_key {
       Key::Ctrl('x') => self.should_quit = true,
       _ => (),
@@ -61,17 +57,9 @@ impl Editor {
    Ok(())
  }
 
- fn read_key() -> Result<Key, std::io::Error> {
-   loop {
-      if let Some(key) = stdin().lock().keys().next() {
-         return key;
-      }
-   }
- }
-
 }
 
 fn die(e: std::io::Error) {
-   print!("{}", termion::clear::All);
+   Terminal::clear_screen();
    panic!("{}", e);
 }
