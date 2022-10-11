e2d6ca6026c7dde1dc43408bc86c5c245d609d26
SUNIL_WSL <shrest464760@gmail.com>
2022-10-11 22:55:20 +0545

    Added a hard coded hello world string

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+mod document;
 mod editor;
+mod row;
 mod terminal;
+pub use document::Document;
 use editor::Editor;
 pub use terminal::Terminal;
 pub use editor::Position;
-
+pub use row::Row;
 fn main() {
     Editor::new().run();
 }
